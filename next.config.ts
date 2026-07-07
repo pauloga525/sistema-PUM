8292fd1fa6a68dc90c93b08a92504c03419af155
Boris Loja <borisli@uets.edu.ec>
2026-07-07 09:34:25 -0500

    arreglo del next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
   // Permite acceder al servidor de desarrollo desde otra máquina
-  allowedDevOrigins: ["192.168.200.31", "https://cartridge-declined-jvc-stakeholders.trycloudflare.com"],
+  allowedDevOrigins: [
+    "192.168.200.31",
+    "cartridge-declined-jvc-stakeholders.trycloudflare.com",
+  ],
   images: {
     remotePatterns: [
       {
